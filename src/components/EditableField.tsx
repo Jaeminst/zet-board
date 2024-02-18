9b4f9ca615e0108b2fe17f9f6922c49405e0d87c
Lee Jaemin <jm1225@kakao.com>
2024-02-18 16:36:27 +0900

    EditableField 컴포넌트 추가

---
+++ src/components/EditableField.tsx
@@ -0,0 +1,82 @@
+import { useState, FunctionComponent, KeyboardEvent } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Check, X } from 'lucide-react';
+
+type EditableFieldProps = {
+  label: string;
+  value: string;
+  onSave: (newValue: string) => void;
+};
+
+const EditableField: FunctionComponent<EditableFieldProps> = ({ label, value, onSave }) => {
+  const [isEditing, setEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(value);
+
+  const handleEdit = () => {
+    setInputValue(value);
+    setEditing(true);
+  };
+  const handleSave = () => {
+    onSave(inputValue);
+    setEditing(false);
+  };
+  const handleCancel = () => {
+    setEditing(false);
+    setInputValue(value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSave();
+    }
+  };
+
+  return (
+    <div className="relative">
+      {isEditing ? (
+        <form onSubmit={(e) => e.preventDefault()} className="flex items-center gap-2">
+          <label htmlFor="editable-field" className="sr-only">
+            {label}
+          </label>
+          <Input
+            id="editable-field"
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            autoFocus
+            className="w-full h-7 border p-[5px] rounded"
+          />
+          <div className="flex flex-row justify-end absolute mt-2 w-full h-8 top-7 z-10">
+            <Button
+              type="button"
+              onClick={handleSave}
+              className="w-7 h-7 bg-black text-white p-1.5 rounded" // Position the button absolutely to the top right of the parent
+            >
+              <Check />
+            </Button>
+            <Button
+              type="button"
+              onClick={handleCancel}
+              className="w-7 h-7 bg-black text-white p-1.5 ml-1 rounded" // Position the button absolutely to the top right of the parent
+            >
+              <X />
+            </Button>
+          </div>
+        </form>
+      ) : (
+        <Button
+          type="button"
+          onClick={handleEdit}
+          className="w-full h-10 bg-white text-black p-0 pl-1.5 justify-start"
+          variant="ghost"
+        >
+          {value || <em>None</em>}
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default EditableField;
--- src/components/database/database-table.tsx
+++ src/components/database/database-table.tsx
@@ -1,10 +1,28 @@
 'use client';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Switch } from '@/components/ui/switch';
+import EditableField from '@/components/EditableField';
 import { useDatabase } from '@/contexts/DatabaseContext';
+import { useEnvironment } from '@/contexts/EnvironmentContext';
+import { Button } from '@/components/ui/button';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
+import { toast } from 'sonner';
 
-export default function DatabaseTable({ databases }: { databases: Database[] }) {
+export default function DatabaseTable({ databases }: { databases: DatabaseList[] }) {
   const [databaseList, setDatabaseList] = useDatabase();
+  const [selectedEnvironment] = useEnvironment();
+
+  const updateDatabaseField = (idx: number, field: keyof DatabaseSetting, value: string) => {
+    const updatedList = databaseList.map((database) => {
+      if (database.idx === idx) {
+        return { ...database, [field]: value };
+      }
+      return database;
+    });
+    setDatabaseList(updatedList);
+    localStorage.setItem(`databaseList_${selectedEnvironment}`, JSON.stringify(updatedList));
+    localStorage.setItem(`databaseSetting_${selectedEnvironment}`, JSON.stringify(updatedList));
+  };
 
   const toggleSwitch = (idx: number) => {
     const updatedList = databaseList.map(database => {
@@ -14,14 +32,26 @@ export default function DatabaseTable({ databases }: { databases: Database[] }) 
       return database;
     });
     setDatabaseList(updatedList);
-    localStorage.setItem("databaseList", JSON.stringify(updatedList));
+    localStorage.setItem(`databaseList_${selectedEnvironment}`, JSON.stringify(updatedList));
+  };
+
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      toast("Copied to clipboard", {
+        description: text,
+        duration: 2000
+      })
+    } catch (err) {
+      alert(`Failed to copy: ${err}`);
+    }
   };
 
   return (
-    <Table className="border shadow-sm rounded-lg p-2">
+    <Table className='border shadow-sm rounded-lg p-2'>
       <TableHeader>
         <TableRow>
-          <TableHead></TableHead>
+          <TableHead>Tunneling</TableHead>
           <TableHead>Alias</TableHead>
           <TableHead>LocalPort</TableHead>
           <TableHead>Identifier</TableHead>
@@ -34,16 +64,47 @@ export default function DatabaseTable({ databases }: { databases: Database[] }) 
       <TableBody>
         {[...databases].sort((a, b) => a.idx - b.idx).map((database) => (
           <TableRow key={database.idx}>
-            <TableCell>
+            <TableCell className='w-24'>
               <Switch checked={database.tunneling} onCheckedChange={() => toggleSwitch(database.idx)} />
             </TableCell>
-            <TableCell>{database.alias}</TableCell>
-            <TableCell>{database.localport}</TableCell>
-            <TableCell>{database.identifier}</TableCell>
-            <TableCell>{database.status}</TableCell>
-            <TableCell>{database.role}</TableCell>
-            <TableCell>{database.engine}</TableCell>
-            <TableCell>{database.size}</TableCell>
+            <TableCell className='w-32 p-1'>
+              <EditableField
+                label="Alias"
+                value={database.alias}
+                onSave={(newValue) => updateDatabaseField(database.idx, 'alias', newValue)}
+              />
+            </TableCell>
+            <TableCell className='w-20 p-1'>
+              <EditableField
+                label="LocalPort"
+                value={database.localport}
+                onSave={(newValue) => updateDatabaseField(database.idx, 'localport', newValue)}
+              />
+            </TableCell>
+            <TableCell className='w-48 p-1'>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      type="button"
+                      onClick={() => copyToClipboard(database.endpoint)}
+                      className="w-full h-10 bg-white text-black p-0 pl-1.5 justify-start"
+                      variant="ghost"
+                    >
+                      {database.identifier}
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Copy to endpoint</p>
+                    <p>{database.endpoint}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </TableCell>
+            <TableCell className='w-16'>{database.status}</TableCell>
+            <TableCell className='w-32'>{database.role}</TableCell>
+            <TableCell className='w-32'>{database.engine}</TableCell>
+            <TableCell className='w-32'>{database.size}</TableCell>
           </TableRow>
         ))}
       </TableBody>
