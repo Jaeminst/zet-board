65cc2a4277d1dfae0800c4356b63e1f83c392255
Lee Jaemin <jm1225@kakao.com>
2024-02-28 00:57:01 +0900

    profileList 변경시 LocalStorage 자동반영

--- src/contexts/ProfileContext.tsx
+++ src/contexts/ProfileContext.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { ipcParser } from '@/lib/ipcPaser';
-import { getLocalStorage } from '@/lib/localStorage';
+import { getLocalStorageProfileList, setLocalStorageProfileList } from '@/lib/localStorage';
 import { useState, createContext, Dispatch, SetStateAction, ReactNode, useContext, useEffect } from 'react';
 
 const ProfileContext = createContext<[Profile[], Dispatch<SetStateAction<Profile[]>>] | undefined>(undefined);
@@ -10,32 +10,24 @@ export function ProfileProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     // 로컬 스토리지에서 프로필 데이터를 로드
-    const initProfiles = getLocalStorage('profileList');
-    if (!initProfiles || Object.keys(initProfiles).length === 0) {
+    const initProfiles = getLocalStorageProfileList();
+    if (!initProfiles || initProfiles.length === 0) {
       // 프로필 데이터가 없거나 비어있으면 초기 프로필 설정 실행
       window.electron.profile.send('init-profiles');
-      window.electron.profile.on('init-profiles', (initProfilesString: string) => {
+      window.electron.profile.once('init-profiles', (initProfilesString: string) => {
         const initProfiles = ipcParser(initProfilesString) as Profile[];
         setProfileList(initProfiles);
-        const profilesObject: ProfileStorage = initProfiles.reduce((acc, profile) => {
-          const { profileName, ...rest } = profile;
-          acc[profileName] = rest;
-          return acc;
-        }, {} as ProfileStorage);
-        localStorage.setItem('profileList', JSON.stringify(profilesObject));
       });
     } else {
       // 저장된 프로필 데이터로 상태 업데이트
-      setProfileList(Object.entries(initProfiles).map(([key, value]) => ({
-        ...value,
-        profileName: key
-      })));
+      setProfileList(initProfiles);
     }
   }, []);
 
   useEffect(() => {
+    setLocalStorageProfileList(profileList);
     console.log(profileList)
-  }, [profileList])
+  }, [profileList]);
 
   return (
     <ProfileContext.Provider value={[profileList, setProfileList]}>
--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,38 +1,20 @@
-export function getLocalStorage(item: string, index?: string) {
-  // localStorage에서 'item' 항목을 가져옵니다.
-  const data = localStorage.getItem(item);
-  // 가져온 데이터가 있다면 JSON으로 파싱합니다.
+export function getLocalStorageProfileList(): Profile[] {
+  const data = localStorage.getItem('profileList');
   if (data) {
-    const parsedData: ProfileStorage = JSON.parse(data);
-    // 'index'가 제공되었고, 해당 'index'가 parsedData 내에 존재하는 경우 해당 데이터를 반환합니다.
-    // 'index'가 제공되지 않았거나 존재하지 않는 경우, parsedData 객체 자체를 반환합니다.
-    return index && index in parsedData ? parsedData[index] : parsedData;
+    const profilesObject: ProfileStorage = JSON.parse(data);
+    return Object.entries(profilesObject).map(([profileName, profileData]) => ({
+      profileName,
+      ...profileData,
+    }));
   }
-  // 데이터가 없다면 null을 반환합니다.
-  return null;
+  return [];
 }
 
-export function setLocalStorage(item: string, index: string, newData?: EditProfileData) {
-  // localStorage에서 전체 item (예: 'profileList')을 가져옵니다.
-  const data = localStorage.getItem(item);
-  if (data) {
-    // 데이터가 존재하면, JSON으로 파싱합니다.
-    const parsedData: ProfileStorage = JSON.parse(data);
-
-    if (!newData) {
-      // newData가 null이면, 해당 인덱스의 데이터를 삭제합니다.
-      delete parsedData[index];
-    } else {
-      // 그렇지 않으면, 특정 인덱스(예: 'dev')에 대한 데이터를 업데이트합니다.
-      // 여기서는 기존 데이터를 유지하면서 새 데이터로 오버라이드합니다.
-      parsedData[index] = { ...parsedData[index], ...newData };
-    }
-
-    // 업데이트된 전체 데이터를 JSON 문자열로 변환하여 다시 localStorage에 저장합니다.
-    localStorage.setItem(item, JSON.stringify(parsedData));
-  } else {
-    // 데이터가 존재하지 않는 경우, 새로운 객체를 생성하여 저장합니다.
-    const initialData = { [index]: newData };
-    localStorage.setItem(item, JSON.stringify(initialData));
-  }
+export function setLocalStorageProfileList(initProfiles: Profile[]) {
+  const profilesObject: ProfileStorage = initProfiles.reduce((acc, profile) => {
+    const { profileName, ...rest } = profile;
+    acc[profileName] = rest;
+    return acc;
+  }, {} as ProfileStorage);
+  localStorage.setItem('profileList', JSON.stringify(profilesObject));
 }
