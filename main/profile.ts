a01a37e60a4b948b4f0fdbd2fd8f47bf48d2f08e
Lee Jaemin <jm1225@kakao.com>
2024-03-07 00:34:23 +0900

    getAwsCredentials 함수 분리

--- main/profile.ts
+++ main/profile.ts
@@ -6,18 +6,13 @@ import { getCaller, assumeRole } from "./aws/stsClient.js";
 import { successMessage } from "./utils/reply.js";
 import { ipcMainListener } from "./utils/ipc.js";
 import { setTimer } from "./utils/setTimer.js";
+import { getAwsCredentials } from "./utils/credentials.js";
 
 interface ConfigureProfile {
   idx?: number;
   profileName: string;
   accountId: string;
   roles: string[];
-}
-
-interface AwsCredentials {
-  accessKeyId: string;
-  secretAccessKey: string;
-  sessionToken?: string;
 }
 
 interface AssumeRoleInput {
@@ -31,38 +26,6 @@ interface AssumeRoleInput {
 // 자격 증명 파일 및 설정 파일 경로
 const credentialsFilePath = join(homedir(), ".aws", "credentials");
 const configFilePath = join(homedir(), ".aws", "config");
-
-async function getAwsCredentials(profile: string): Promise<AwsCredentials> {
-  const content = await fs.readFile(credentialsFilePath, { encoding: 'utf-8' });
-  const lines = content.split(/\r?\n/);
-  let currentProfile = '';
-  let credentials: AwsCredentials | null = null; // 초기값을 null로 설정
-  for (const line of lines) {
-    const profileMatch = line.match(/^\[(.+)\]$/);
-    if (profileMatch) {
-      currentProfile = profileMatch[1];
-    } else if (currentProfile === profile) {
-      const matchAccessKey = line.match(/^aws_access_key_id\s*=\s*(.+)$/);
-      const matchSecretKey = line.match(/^aws_secret_access_key\s*=\s*(.+)$/);
-      const matchSessionToken = line.match(/^aws_session_token\s*=\s*(.+)$/);
-      if (matchAccessKey) {
-        credentials = credentials || { accessKeyId: '', secretAccessKey: '' }; // 객체가 아직 없으면 생성
-        credentials.accessKeyId = matchAccessKey[1];
-      } else if (matchSecretKey) {
-        credentials = credentials || { accessKeyId: '', secretAccessKey: '' }; // 객체가 아직 없으면 생성
-        credentials.secretAccessKey = matchSecretKey[1];
-      } else if (matchSessionToken) {
-        credentials = credentials || { accessKeyId: '', secretAccessKey: '' }; // 객체가 아직 없으면 생성
-        credentials.sessionToken = matchSessionToken[1]; // sessionToken을 저장합니다.
-      }
-    }
-  }
-
-  if (!credentials || !credentials.accessKeyId || !credentials.secretAccessKey) {
-    throw new Error(`Credentials not found for profile: ${profile}`);
-  }
-  return credentials;
-}
 
 // 프로파일 정보 조회
 async function initProfile(profile: string) {
