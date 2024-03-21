30cc2ff78216287d4608de7b2fff728f1855e7d4
Lee Jaemin <jm1225@kakao.com>
2024-03-22 02:06:21 +0900

    editable-field cn merge

--- src/components/ui/editable-field.tsx
+++ src/components/ui/editable-field.tsx
@@ -1,15 +1,26 @@
-import { useState,type FunctionComponent,type KeyboardEvent } from 'react';
+"use client"
+
+import { useState, type FunctionComponent, type KeyboardEvent } from 'react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Check, X } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 type EditableFieldProps = {
   label: string;
   value: string;
   onSave: (newValue: string) => void;
+  className?: string;
+  disabled?: boolean;
 };
 
-export const EditableField: FunctionComponent<EditableFieldProps> = ({ label, value, onSave }) => {
+export const EditableField: FunctionComponent<EditableFieldProps> = ({
+  label,
+  value,
+  onSave,
+  className,
+  disabled = false
+}) => {
   const [isEditing, setEditing] = useState(false);
   const [inputValue, setInputValue] = useState(value);
 
@@ -46,16 +57,23 @@ export const EditableField: FunctionComponent<EditableFieldProps> = ({ label, va
             onChange={(e) => setInputValue(e.target.value)}
             onKeyDown={handleKeyDown}
             autoFocus
-            className="w-full h-7 border p-[5px] rounded"
+            className={cn(
+              "w-full h-8 border p-[5px] rounded",
+              className
+            )}
+            disabled={disabled}
           />
-          <div className="flex flex-row justify-end absolute mt-2 w-full h-8 top-7 z-10">
-            <Button
-              type="button"
-              onClick={handleSave}
-              className="w-7 h-7 bg-black text-white p-1.5 rounded" // Position the button absolutely to the top right of the parent
-            >
-              <Check />
-            </Button>
+          <div className="flex flex-row justify-end absolute mt-2 w-full h-8 top-8 z-10">
+            {disabled
+              ? <></>
+              : <Button
+                type="button"
+                onClick={handleSave}
+                className="w-7 h-7 bg-black text-white p-1.5 rounded" // Position the button absolutely to the top right of the parent
+              >
+                <Check />
+              </Button>
+            }
             <Button
               type="button"
               onClick={handleCancel}
@@ -69,7 +87,10 @@ export const EditableField: FunctionComponent<EditableFieldProps> = ({ label, va
         <Button
           type="button"
           onClick={handleEdit}
-          className="w-full h-10 bg-white text-black p-0 pl-1.5 justify-start"
+          className={cn(
+            "w-full h-10 bg-white text-black p-0 pl-1.5 justify-start",
+            className
+          )}
           variant="ghost"
         >
           {value || <em>None</em>}
